f6a184053a7d2ea27d2354e5a569b3915a4a93ab
jiebinzhou98 <benzhou98@outlook.com>
2025-05-26 11:39:15 -0400

    changing the Home page explore button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,13 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center min-h-screen gap-6 text-center">
         <h1 className="text-3xl font-bold">Welcome to Blog Platform ✍️
           <p className="text-gray-500 text-lg">Explore what others have published or log in to create your own
+            <div className="mt-4">
               <Button
                 className="bg-gradient-to-r from-blue-500 to-indigo-500 text-white px-8 py-4 text-lg rounded-full shadow-lg hover:brightness-110 transition-all" 
-                onClick={() => router.push('/explore')}>Explore Posts</Button>
+                onClick={() => router.push('/explore')}>Explore Posts
+              </Button>
+            </div>
+
           </p>
         </h1>
       </main>
