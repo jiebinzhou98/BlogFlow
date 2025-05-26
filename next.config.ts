9739bd197812a7b745fb7783b12484d482db4a37
jiebinzhou98 <benzhou98@outlook.com>
2025-05-26 09:08:15 -0400

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint:{
+    ignoreDuringBuilds: true, // Ignore ESLint errors during build
+  },
   images: {
     remotePatterns: [
       {
